fbe3201d5d76fc407ba60b3b3b1fa49d90b2acc7
Tobi-Ferretti <stfertob@bx.fallmerayer.it>
2020-12-09 10:51:26 +0100

    Commit 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,21 @@
 import org.soulwing.snmp.*;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
+
+        Mib mib = MibFactory.getInstance().newMib();
+        mib.load("SNMPv2-MIB");
 
         SimpleSnmpV2cTarget target = new SimpleSnmpV2cTarget();
         target.setAddress("10.0.0.1");
         target.setCommunity("public");
 
-        SnmpContext context = SnmpFactory.getInstance().newContext(target);
+        SnmpContext context = SnmpFactory.getInstance().newContext(target, mib);
         try {
-            // perform some SNMP operations
+            VarbindCollection result = context.getNext("sysUpTime").get();
+            System.out.println(result.get("sysUpTime"));
         }
         finally {
             context.close();
